94a3cd82917ca8951ee1f53ccf36192bf5c1ba7e
Arif_1 <uarif1933@gmail.com>
2024-12-04 16:48:46 +0600

    Server connect to database

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5500;
- 
+
 // middleware start
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 // middle end
 // mongodb database connect and also operation here
 
-
-const uri = `mongodb+srv://${process.env.UserName}:${process.env.UserPassword}@cluster0.wclmi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://MoviePortal:lfT332UREnHY9VDB@cluster0.wclmi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,27 +20,37 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+// Connect the client to the server	(optional starting in v4.7)
+// await client.connect();
+// Send a ping to confirm a successful connection
+// await client.db("admin").command({ ping: 1 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // database create
+    const movieCollection = client.db("movieDB").collection("movies");
+    app.post("/addmovie", async (req, res) => {
+      const add_Movie = req.body;
+      const result = await movieCollection.insertOne(add_Movie);
+      console.log(result);
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
 
+// Ensures that the client will close when you finish/error
+// await client.close();
 
 // mongodb database connect and also operation here
-
-
 
 // app.get('/',(req,res)=>{
 //   res.send("Assalamualaikm,")
